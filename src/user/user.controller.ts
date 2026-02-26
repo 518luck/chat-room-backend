c928d4444e64749f11e6205f4faab9949efbd792
518luck <165388484+518luck@users.noreply.github.com>
2026-02-26 23:06:45 +0800

    feat(user): 添加用户信息更新功能
    - 创建 UpdateUserDto 用于更新用户信息验证
    - 在 UserService 中实现 update 方法，支持更新昵称和头像
    - 在 UserController 中添加更新用户信息接口和验证码发送接口
    - 添加更新密码验证码发送接口

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Inject, Post, Query } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Inject,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { UserService } from '@/user/user.service';
 import { RegisterUserDto } from '@/user/dto/register-user.dto';
 import { RedisService } from '@/redis/redis.service';
@@ -7,6 +15,7 @@ import { LoginUserDto } from '@/user/dto/login-user.dto';
 import { JwtService } from '@nestjs/jwt';
 import { RequireLogin, UserInfo } from '@/custom.decorator';
 import { UpdateUserPasswordDto } from '@/user/dto/update-user-password.dto';
+import { UpdateUserDto } from '@/user/dto/udpate-user.dto';
 
 @Controller('user')
 export class UserController {
@@ -72,5 +81,59 @@ export class UserController {
   @Post('update_password')
   updatePassword(@Body() passwordDto: UpdateUserPasswordDto) {
     return this.userService.updatePassword(passwordDto);
+  }
+
+  // 获取更新密码验证码
+  @Get('update_password/captcha')
+  async updatePasswordCaptcha(@Query('address') address: string) {
+    if (!address) {
+      throw new BadRequestException('邮箱地址不能为空');
+    }
+    const code = Math.random().toString().slice(2, 8);
+
+    await this.redisService.set(
+      `update_password_captcha_${address}`,
+      code,
+      10 * 60,
+    );
+
+    await this.emailService.sendMail({
+      to: address,
+      subject: '更改密码验证码',
+      html: `<p>你的更改密码验证码是 ${code}</p>`,
+    });
+    return '发送成功';
+  }
+
+  // 更新用户信息
+  @Post('update')
+  @RequireLogin()
+  async update(
+    @UserInfo('userId') userId: number,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    return await this.userService.update(userId, updateUserDto);
+  }
+
+  // 获取更新用户信息验证码
+  @Get('update/captcha')
+  async updateCaptcha(@Query('address') address: string) {
+    if (!address) {
+      throw new BadRequestException('邮箱地址不能为空');
+    }
+    const code = Math.random().toString().slice(2, 8);
+
+    await this.redisService.set(
+      `update_user_captcha_${address}`,
+      code,
+      10 * 60,
+    );
+
+    await this.emailService.sendMail({
+      to: address,
+      subject: '更改用户信息验证码',
+      html: `<p>你的验证码是 ${code}</p>`,
+    });
+    return '发送成功';
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -10,6 +10,7 @@ import { RedisService } from '@/redis/redis.service';
 import { RegisterUserDto } from '@/user/dto/register-user.dto';
 import { LoginUserDto } from '@/user/dto/login-user.dto';
 import { UpdateUserPasswordDto } from '@/user/dto/update-user-password.dto';
+import { UpdateUserDto } from '@/user/dto/udpate-user.dto';
 
 @Injectable()
 export class UserService {
@@ -146,6 +147,52 @@ export class UserService {
     } catch (e) {
       this.logger.error(e, UserService);
       return '密码修改失败';
+    }
+  }
+
+  // 更新用户信息
+  async update(userId: number, updateUserDto: UpdateUserDto) {
+    const captcha = await this.redisService.get(
+      `update_user_captcha_${updateUserDto.email}`,
+    );
+
+    if (!captcha) {
+      throw new HttpException('验证码已失效', HttpStatus.BAD_REQUEST);
+    }
+
+    if (updateUserDto.captcha !== captcha) {
+      throw new HttpException('验证码不正确', HttpStatus.BAD_REQUEST);
+    }
+
+    const foundUser = await this.prismaService.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!foundUser) {
+      throw new HttpException('用户不存在', HttpStatus.BAD_REQUEST);
+    }
+
+    if (updateUserDto.nickName) {
+      foundUser.nickName = updateUserDto.nickName;
+    }
+
+    if (updateUserDto.headPic) {
+      foundUser.headPic = updateUserDto.headPic;
+    }
+
+    try {
+      await this.prismaService.user.update({
+        where: {
+          id: userId,
+        },
+        data: foundUser,
+      });
+      return '用户信息修改成功';
+    } catch (e) {
+      this.logger.error(e, UserService);
+      return '用户信息修改成功';
     }
   }
 }
